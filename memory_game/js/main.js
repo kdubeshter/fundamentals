41f87425243f5c03f15e7ce97b84146d5573b04e
Kayle DuBeshter <kayledubeshter@gmail.com>
2018-12-01 09:25:34 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped" + cardTwo);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+	alert("Sorry, try again.");
+	}
+}
+
